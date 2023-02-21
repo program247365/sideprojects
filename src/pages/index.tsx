f7131663460ea429dc6c7df767fb235362ef85eb
Kevin B. Ridgway <k@kbr.sh>
2023-02-20 23:04:28 -0500

    Adding nice macOS titlebar that is draggable, style changes.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,5 @@
 import { useState, useEffect } from "react";
 import { invoke } from "@tauri-apps/api/tauri";
-type domain = {
-  id: string;
-  url: string;
-};
 
 function App() {
   const [inputValue, setInputValue] = useState("");
@@ -58,7 +54,8 @@ function App() {
   }, []);
 
   return (
-    <div className="p-4">
+    <div className="p-4 pt-[40px]">
+      <div data-tauri-drag-region className="titlebar"></div>
       <div className="flex flex-col justify-center align-center">
         <div className="flex flex-row items-center justify-between w-full pb-6">
           <input
@@ -71,35 +68,33 @@ function App() {
             placeholder="Search projects..."
           />
         </div>
-        <table className="w-full h-[500px] overflow-auto mb-4">
-          <tbody>
-            {domains &&
-              domains.map((d) => {
-                return (
-                  <tr
-                    key={d!.id}
-                    className="flex flex-row justify-items-center items-center h-16 justify-between p-4 rounded-lg"
-                  >
-                    <td className="w-full">
-                      <a
-                        className="w-full color-[#010101] font-bold text-2xl tracking-wide group text-[#010101] transition-all duration-300 ease-in-out"
-                        href={`https://${d!.url}`}
-                        rel="noopener"
-                        target="_blank"
-                      >
-                        <span className="bg-left-bottom bg-gradient-to-r from-green-500 to-green-500 bg-[length:0%_2px] bg-no-repeat group-hover:bg-[length:100%_2px] transition-all duration-500 ease-out">
-                          {d!.url}
-                        </span>
-                      </a>
-                      <p className="w-full font-light text-[#8D8D8D]">
-                        Expires in 165 days
-                      </p>
-                    </td>
-                  </tr>
-                );
-              })}
-          </tbody>
-        </table>
+        <div className="pt-4 pb-4 w-full mb-4 h-[500px] overflow-y-scroll no-scrollbar">
+          {domains &&
+            domains.map((d) => {
+              return (
+                <div
+                  key={d!.id}
+                  className="flex flex-row justify-items-center items-center h-16 justify-between p-4 rounded-lg"
+                >
+                  <span className="w-full">
+                    <a
+                      className="w-full color-[#010101] font-bold text-2xl tracking-wide group text-[#010101] transition-all duration-300 ease-in-out"
+                      href={`https://${d!.url}`}
+                      rel="noopener"
+                      target="_blank"
+                    >
+                      <span className="bg-left-bottom bg-gradient-to-r from-green-500 to-green-500 bg-[length:0%_2px] bg-no-repeat group-hover:bg-[length:100%_2px] transition-all duration-500 ease-out">
+                        {d!.url}
+                      </span>
+                    </a>
+                    <p className="w-full font-light text-[#8D8D8D]">
+                      Expires in 165 days
+                    </p>
+                  </span>
+                </div>
+              );
+            })}
+        </div>
         <form>
           <label>
             URL:
